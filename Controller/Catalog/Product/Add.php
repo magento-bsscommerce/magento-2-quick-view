c622b250aa3b0f084656689fbbb20d0d8275bb60
manhnd <manhnd@bsscommerce.com>
2020-04-06 13:22:39 +0700

    Fix Direct Use of object manager( BSS-EXM2-QV-28)

--- Controller/Catalog/Product/Add.php
+++ Controller/Catalog/Product/Add.php
@@ -15,7 +15,13 @@
  * @copyright  Copyright (c) 2019-2020 BSS Commerce Co. ( http://bsscommerce.com )
  * @license    http://bsscommerce.com/Bss-Commerce-License.txt
  */
+
 namespace Bss\Quickview\Controller\Catalog\Product;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\ViewModel\Product\Checker\AddToCompareAvailability;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Framework\View\Result\PageFactory;
 
 /**
  * Class Add
@@ -28,6 +34,65 @@ class Add extends \Magento\Catalog\Controller\Product\Compare\Add
      * @var ObjectManagerInterface
      */
     protected $myObjectManager;
+    /**
+     * @var \Magento\Framework\Escaper
+     */
+    protected $escaper;
+    /**
+     * @var \Magento\Catalog\Helper\Product\Compare
+     */
+    protected $compare;
+
+    /**
+     * Add constructor.
+     * @param \Magento\Framework\Escaper $escaper
+     * @param \Magento\Catalog\Helper\Product\Compare $compare
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Catalog\Model\Product\Compare\ItemFactory $compareItemFactory
+     * @param \Magento\Catalog\Model\ResourceModel\Product\Compare\Item\CollectionFactory $itemCollectionFactory
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Customer\Model\Visitor $customerVisitor
+     * @param \Magento\Catalog\Model\Product\Compare\ListCompare $catalogProductCompareList
+     * @param \Magento\Catalog\Model\Session $catalogSession
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param Validator $formKeyValidator
+     * @param PageFactory $resultPageFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param AddToCompareAvailability|null $compareAvailability
+     */
+    public function __construct(
+        \Magento\Framework\Escaper $escaper,
+        \Magento\Catalog\Helper\Product\Compare $compare,
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Catalog\Model\Product\Compare\ItemFactory $compareItemFactory,
+        \Magento\Catalog\Model\ResourceModel\Product\Compare\Item\CollectionFactory $itemCollectionFactory,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Customer\Model\Visitor $customerVisitor,
+        \Magento\Catalog\Model\Product\Compare\ListCompare $catalogProductCompareList,
+        \Magento\Catalog\Model\Session $catalogSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        Validator $formKeyValidator,
+        PageFactory $resultPageFactory,
+        ProductRepositoryInterface $productRepository,
+        AddToCompareAvailability $compareAvailability = null
+    ) {
+        $this->compare = $compare;
+        $this->escaper = $escaper;
+        parent::__construct(
+            $context,
+            $compareItemFactory,
+            $itemCollectionFactory,
+            $customerSession,
+            $customerVisitor,
+            $catalogProductCompareList,
+            $catalogSession,
+            $storeManager,
+            $formKeyValidator,
+            $resultPageFactory,
+            $productRepository,
+            $compareAvailability
+        );
+    }
 
     /**
      * Add product
@@ -54,12 +119,12 @@ class Add extends \Magento\Catalog\Controller\Product\Compare\Add
 
             if ($product) {
                 $this->_catalogProductCompareList->addProduct($product);
-                $productName = $this->myObjectManager->get('Magento\Framework\Escaper')->escapeHtml($product->getName());
-                $this->messageManager->addSuccess(__('You added product %1 to the comparison list.', $productName));
+                $productName = $this->escaper->escapeHtml($product->getName());
+                $this->messageManager->addSuccessMessage(__('You added product %1 to the comparison list.', $productName));
                 $this->_eventManager->dispatch('catalog_product_compare_add_product', ['product' => $product]);
             }
 
-            $this->myObjectManager->get('Magento\Catalog\Helper\Product\Compare')->calculate();
+            $this->compare->calculate();
         }
         $params = $this->getRequest()->getParams();
         if (isset($params['bssquickview']) && $params['bssquickview'] == 1) {
